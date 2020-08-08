2583ce58eff57cb77816ce3cc69fd664411e2a7c
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-08 19:34:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,5 +13,14 @@ class Transfer
     sender.valid? && receiver.valid?
   end 
   
-  
+  def execute_transaction
+    if self.valid? && @status == "pending" && sender.balance > @amount
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
 end
