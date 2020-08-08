7c02adc21c0d8048b0e6d1cf5c9b4689ab32e911
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-08-08 19:35:10 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,4 +8,20 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    return "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == "open"
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end 
